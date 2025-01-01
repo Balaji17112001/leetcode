7315f55ddcf5f510b4ae80421e1a675df432f0f3
Balaji17112001 <143730336+Balaji17112001@users.noreply.github.com>
2025-01-01 22:46:20 +0530

    Time: 10 ms (6.65%) | Memory: 45.2 MB (5.06%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,34 +1,33 @@
 class Solution {
-    public int maxScore(String s) {
-        
+    public int maxScore(String s) 
+    {
         String str = "";
-        String ss = "";
+        String str2 = "";
         int max = Integer.MIN_VALUE;
         for(int i=0;i<s.length()-1;i++)
         {
             str = s.substring(0,i+1);
-            ss = s.substring(i+1,s.length());
-
-            int countright = 0;
+            int countleft = 0; 
             for(int j=0;j<str.length();j++)
             {
                 if(str.charAt(j) == '0')
-                {
-                    countright++;
-                }
-            }
-
-            int countleft = 0;
-            for(int j=0;j<ss.length();j++)
-            {
-                if(ss.charAt(j) == '1')
                 {
                     countleft++;
                 }
             }
 
-            max = Math.max(max,(countright+countleft));
+            str2 = s.substring(i+1,s.length());
+            int countright = 0;
+            for(int j=0;j<str2.length();j++)
+            {
+                if(str2.charAt(j) == '1')
+                {
+                    countright++;
+                }
+            }
+
+            max = Math.max(max,(countleft + countright));
         }
-        return max;
+        return max;    
     }
 }
