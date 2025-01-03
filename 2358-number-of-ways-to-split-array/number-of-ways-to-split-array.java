0f086384c052cb6c27972aca400a27ad0a0a8631
Balaji17112001 <143730336+Balaji17112001@users.noreply.github.com>
2025-01-03 22:51:31 +0530

    Time: 3 ms (59.69%) | Memory: 61.3 MB (10.46%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,6 +1,25 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long[] leftsum = new long[nums.length-1];
+        long[] prefixsum = new long[nums.length];
+        prefixsum[0] = nums[0];
+
+        for(int i=1;i<nums.length;i++)
+        {
+            prefixsum[i] = prefixsum[i-1] + nums[i];
+        }
+
+        int count = 0;
+
+        for(int i=0;i<prefixsum.length-1;i++)
+        {
+            if(prefixsum[i] >= (prefixsum[prefixsum.length-1] - prefixsum[i]))
+            {
+                count++;
+            }
+        }
+
+        return count;
+        /*long[] leftsum = new long[nums.length-1];
         leftsum[0] = nums[0];
 
         long[] rightsum = new long[nums.length-1];
@@ -25,6 +44,6 @@ class Solution {
                 count++;
             }
         }
-        return count;
+        return count;*/
     }
 }
